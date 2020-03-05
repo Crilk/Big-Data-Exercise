82c1f684c69bb5de54a0cd52e16617e103fd6263
Dongjie Qiu <evercrilk@outlook.com>
2020-03-05 15:31:44 +0000

    update code for w2e2

--- w2e3/mapper.py
+++ w2e3/mapper.py
@@ -3,3 +3,12 @@ import sys
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+    words = line.split()
+    for index in range(0, int(len(words) / 5)):
+        key = words[index * 5] + '-' + words[index * 5 + 1]
+        value = words[index * 5 + 2] + '-' + words[index * 5 + 3] + '-' + words[index * 5 + 4]
+
+        print('%s\t%s' % (key, value))
--- w2e3/reducer.py
+++ w2e3/reducer.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python
+from operator import itemgetter
 import sys
+
+current_key = None
+current_value = None
+key = None
+
+# input comes from STDIN
+for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
+        # parse the input we got from mapper.py
+        key, value = line.split('\t', 1)
+        # this IF-switch only works because Hadoop sorts map output
+        # by key (here: word) before it is passed to the reducer
+        if current_key == key:
+            current_value = current_value + '-' + value
+        else:
+            if current_key:
+                # write result to STDOUT
+                print('%s\t%s' % (current_key, current_value))
+            current_value = value
+            current_key = key
+
+        # do not forget to output the last word if needed!
+if current_key == key:
+    print('%s\t%s' % (current_key, current_value))

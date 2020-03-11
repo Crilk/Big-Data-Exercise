cd5d9a65798705cfabd4415cf2175db8cb6995e4
Dongjie Qiu <evercrilk@outlook.com>
2020-03-11 10:42:41 +0000

    update the code for w2e3

--- w2e3/mapper.py
+++ w2e3/mapper.py
@@ -10,8 +10,8 @@ for line in sys.stdin:
     line = line.strip()
     # split the line into words
     words = line.split()
-    for index in range(0, int(len(words) / 5)):
-        key = words[index * 5] + '-' + words[index * 5 + 1]
-        value = words[index * 5 + 2] + ',' + words[index * 5 + 3] + ',' + words[index * 5 + 4]
 
-        print('%s\t%s\t%s' % (key, value, filename))
+    key = words[0] + '-' + words[1]
+    value = words[2] + ',' + words[3] + ',' + words[4]
+
+    print('%s\t%s\t%s' % (key, value, filename))
--- w2e4/reducer.py
+++ w2e4/reducer.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 import sys
 
-
 last_group = None
 # input comes from STDIN
 for line in sys.stdin:
-        # remove leading and trailing whitespace
-        val = line.strip()
+    # remove leading and trailing whitespace
+    val = line.strip()
 
-	(month,temp) = val.split(",")
-	group = month
-	if last_group != group:
-		print val
-		last_group = group
+    (month, temp) = val.split(",")
+    group = month
+    if last_group != group:
+        print(val)
+        last_group = group

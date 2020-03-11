a4a33653351b8b2593b473edb61f7fe29b16a84b
Dongjie Qiu <evercrilk@outlook.com>
2020-03-11 10:58:46 +0000

    update mapper.py to remove the first line

--- w2e3/mapper.py
+++ w2e3/mapper.py
@@ -2,10 +2,16 @@
 import sys
 import os
 
+doneFirst = 0
 filepath = os.environ["map_input_file"]
 filename = os.path.split(filepath)[-1]
 # input comes from STDIN (standard input)
 for line in sys.stdin:
+    # ignore the first line of data (the header line)
+    if doneFirst == 0:
+        doneFirst = 1
+        continue
+
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words

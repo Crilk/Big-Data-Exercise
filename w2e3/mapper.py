ad153c5804495b259bab6b9190c5c6c20b6053e7
Dongjie Qiu <evercrilk@outlook.com>
2020-03-17 16:17:35 +0000

    update the code for w2e3

--- w2e3/mapper.py
+++ w2e3/mapper.py
@@ -2,22 +2,34 @@
 import sys
 import os
 
-doneFirst = 0
-filepath = os.environ["map_input_file"]
-filename = os.path.split(filepath)[-1]
+# get input file name
+input_name = os.environ['mapreduce_map_input_file']
+
+# using name of input file figure out if airport is Heathrow or Wick
+# store this information as a loc = H or loc = W
+if 'heathrow' in input_name:
+    loc = 'H'
+else:
+    loc = 'W'
+
 # input comes from STDIN (standard input)
 for line in sys.stdin:
-    # ignore the first line of data (the header line)
-    if doneFirst == 0:
-        doneFirst = 1
-        continue
-
     # remove leading and trailing whitespace
     line = line.strip()
-    # split the line into words
-    words = line.split()
 
-    key = words[0] + '-' + words[1]
-    value = words[2] + ',' + words[3] + ',' + words[4]
+    # if line contains 'yyyy' in it remove it (it is header line)
+    if 'yyyy' in line:
+        continue
 
-    print('%s\t%s\t%s' % (key, value, filename))
+    # split line into year, month, tmax, tmin and rain variables
+    year, month, tmax, tmin, rain = line.split()
+
+    # create key to be 'year-month-loc'
+    key = '%s-%s-%s' % (year, month, loc)
+
+    # write the results to STDOUT (standard output);
+    # what we output here will be the input for the
+    # Reduce step, i.e. the input for reducer.py
+    #
+    # tab-delimited;
+    print('%s\t%s\t%s\t%s' % (key, tmax, tmin, rain))
--- w2e3/reducer.py
+++ w2e3/reducer.py
@@ -2,31 +2,58 @@
 from operator import itemgetter
 import sys
 
-current_key = None
-current_value = None
-current_label = None
-key = None
+# print header line
+print('yyyy\tmm\tHtmax\tHtmin\tHrain\tWtmax\tWtmin\tWrain')
+
+current_date = None
+current_loc = None
+current_output = None
+date = None
+loc = None
+test = 0  # this variable is to keep track if we have come to times when both H and W $
 
 # input comes from STDIN
 for line in sys.stdin:
-        # remove leading and trailing whitespace
-        line = line.strip()
-        # parse the input we got from mapper.py
-        key, value, label = line.split('\t', 2)
-        # this IF-switch only works because Hadoop sorts map output
-        # by key (here: word) before it is passed to the reducer
-        if current_key == key:
-            if label == "wickairportdata.txt":
-                current_value = current_value + '|' + value
-            elif label == "heathrowdata.txt":
-                current_value = value + '|' + current_value
-        else:
-            if current_key:
-                # write result to STDOUT
-                print('%s\t%s' % (current_key, current_value))
-            current_value = value
-            current_key = key
+    # remove leading and trailing whitespace
+    line = line.strip()
 
-# do not forget to output the last word if needed!
-if current_key == key:
-    print('%s\t%s' % (current_key, current_value))
+    # parse the input we got from mapper.py
+    key, tmax, tmin, rain = line.split()
+
+    # split key into year, month and location
+    year, month, loc = key.split('-')
+
+    # create date
+    date = '%s-%s' % (year, month)
+
+    # this If-switch only works because Hadoop sorts map output
+    # by key (here year-month-loc) before it is passed to the reducer
+    if current_date == date:
+        # this means data for both airports exists for this date
+        # write results to STDOUT
+        print('%s\t%s\t' % (year, month) + current_output + '\t%s\t%s\t%s' % (tmax, tmin, rain))
+
+        # update current data
+        current_date = date
+        current_loc = loc
+        current_output = '%s\t%s\t%s' % (tmax, tmin, rain)
+
+        # Set test variable to 1 so that same data is not printed twice
+        test = 1
+    else:
+        # this means that the new date is different from previous
+        # need to figure out if this is a change from W to W or from W to H
+        if current_loc == loc:
+            # this means we have a change from W to W i.e. no H data is p$
+            # write results to STDOUT
+            print('%s\t%s\t-\t-\t-\t' % (current_date.split('-')[0], current_date.split('-')[1]))
+
+            # update current data
+            current_loc = loc
+            current_date = date
+            current_output = '%s\t%s\t%s' % (tmax, tmin, rain)
+        else:
+            # current_loc is empty so set up current data
+            current_loc = loc
+            current_date = date
+            current_output = '%s\t%s\t%s' % (tmax, tmin, rain)
